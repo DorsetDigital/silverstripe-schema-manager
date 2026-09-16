efb0860db219267304bc25f98b8f9c3c078ab664
Tim <DorsetDigital@users.noreply.github.com>
2026-09-16 13:58:55 +0100

    Add product schema builder

---
+++ src/Model/Schema/ProductSchema.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace DorsetDigital\SchemaManager\Model\Schema;
+
+use SilverStripe\Control\Director;
+
+class ProductSchema extends Schema
+{
+    public static function create(string $url, string $name, ?string $description = null): static
+    {
+        $url = rtrim($url, '/') . '/';
+
+        $data = [
+            '@type' => 'Product',
+            '@id' => $url . '#product',
+            'url' => $url,
+            'name' => $name,
+            'mainEntityOfPage' => [
+                '@id' => $url . '#webpage',
+            ],
+        ];
+
+        if ($description) {
+            $data['description'] = $description;
+        }
+
+        return new static($data);
+    }
+
+    public function setImage(?string $url): static
+    {
+        if ($url) {
+            $this->data['image'] = $url;
+        }
+
+        return $this;
+    }
+
+    public function setOffer(
+        float|int|string $price,
+        string $currency,
+        ?bool $inStock = null,
+        ?string $url = null
+    ): static {
+        $offer = [
+            '@type' => 'Offer',
+            'priceCurrency' => $currency,
+            'price' => $price,
+        ];
+
+        if ($url) {
+            $offer['url'] = $url;
+        } elseif (!empty($this->data['url'])) {
+            $offer['url'] = $this->data['url'];
+        }
+
+        if ($inStock !== null) {
+            $offer['availability'] = $inStock
+                ? 'https://schema.org/InStock'
+                : 'https://schema.org/OutOfStock';
+        }
+
+        $this->data['offers'] = $offer;
+
+        return $this;
+    }
+
+    public function setSKU(?string $sku): static
+    {
+        if ($sku) {
+            $this->data['sku'] = $sku;
+        }
+
+        return $this;
+    }
+
+    public function setBrand(?string $name): static
+    {
+        if ($name) {
+            $this->data['brand'] = [
+                '@type' => 'Brand',
+                'name' => $name,
+            ];
+        }
+
+        return $this;
+    }
+}
