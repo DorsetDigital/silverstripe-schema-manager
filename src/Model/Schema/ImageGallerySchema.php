fe09560e062ddfb47f7de22f9803f26d0425546a
Tim <DorsetDigital@users.noreply.github.com>
2026-09-18 21:59:42 +0200

    Add ImageGallery schema support (#9)
    * Add ImageGallery schema builder
    
    * Use hasPart for ImageGallery images
    
    * Document ImageGallery schema support

---
+++ src/Model/Schema/ImageGallerySchema.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace DorsetDigital\SchemaManager\Model\Schema;
+
+class ImageGallerySchema extends Schema
+{
+    public static function create(string $url, ?string $name = null, ?string $description = null): static
+    {
+        $url = rtrim($url, '/') . '/';
+
+        $data = [
+            '@type' => 'ImageGallery',
+            '@id' => $url . '#imagegallery',
+            'url' => $url,
+            'mainEntityOfPage' => [
+                '@id' => $url . '#webpage',
+            ],
+            'hasPart' => [],
+        ];
+
+        if ($name) {
+            $data['name'] = $name;
+        }
+
+        if ($description) {
+            $data['description'] = $description;
+        }
+
+        return new static($data);
+    }
+
+    public function addImage(
+        string $url,
+        ?string $name = null,
+        ?string $description = null,
+        ?int $width = null,
+        ?int $height = null
+    ): static {
+        $image = [
+            '@type' => 'ImageObject',
+            'contentUrl' => $url,
+        ];
+
+        if ($name) {
+            $image['name'] = $name;
+        }
+
+        if ($description) {
+            $image['description'] = $description;
+        }
+
+        if ($width) {
+            $image['width'] = $width;
+        }
+
+        if ($height) {
+            $image['height'] = $height;
+        }
+
+        $this->data['hasPart'][] = $image;
+
+        return $this;
+    }
+
+    public function setThumbnail(?string $url): static
+    {
+        if ($url) {
+            $this->data['thumbnailUrl'] = $url;
+        }
+
+        return $this;
+    }
+}
